39be234944c2d5192c4cf76670d3d16c08d58844
aakash <aakashbhowmick@gmail.com>
2024-08-25 18:32:57 +0530

    First commit of JsonWriter

---
+++ src/main/java/org/example/JsonWriter.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class JsonWriter {
+
+    // Constraints : Input object contains members of the following type only:
+    // - String
+    // - Integer
+    // - Double
+    // - List
+    public static String toJson(Object o) {
+
+        Class klass = o.getClass();
+        return null;
+    }
+}
